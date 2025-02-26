48fc9d1e1cb51bc9f53a876a72ae215f4027f10b
LeoSatCode <your-email@example.com>
2025-02-26 16:47:42 -0300

    final touch on button

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -1,17 +1,27 @@
 import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
+  
+  @font-face {
+  font-family: "Kailasa";
+  src: url("../../assets/fonts/kailasa-bold.ttf") format("truetype");
+  }
+
+  font-family: "Kailasa", sans-serif;
+  font-size: 16px;
+  color: #FFFFFF;
+
   border: 1px solid #FAFAFA;
   border-radius: 20px;
-  background-color: #ffffff;
+  background-color: #FAFAFA60;
 
-  height: 62px;
-  width: 80%;
+  min-height: 62px;
+  width: 50%;
 
   margin: 50px;
 
   &:hover {
-    background-color: #FAFAFA40;
+    background-color: #FAFAFA90;
     cursor: pointer;
   }
 `;
